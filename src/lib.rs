73ba6d1a542ded98d7999ecf1817e39f493eec38
Sam <samuel.grulich@gmail.com>
2023-03-19 10:37:58 +0100

    Lib update
    added functions for easier setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use winit::{
     event::*,
-    event_loop::EventLoop,
-    window::WindowBuilder,
+    event_loop::{EventLoop, ControlFlow},
+    window::{WindowBuilder, Window},
 };
 
 mod modules;
@@ -10,6 +10,73 @@ pub use modules::*;
 
 const PARTICLE_SIZE: f32 = 0.1;
 const GRID_DIMENSIONS: (u32, u32) = (20, 20);
+
+
+pub struct InitOutput {
+    pub event_loop: EventLoop<()>,
+    pub window: Window,
+    pub width: u32,
+    pub height: u32,
+    pub aspect_ratio: f32,
+}
+
+pub fn init() -> InitOutput {
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_resizable(false)
+        .build(&event_loop)
+        .unwrap();
+
+    let winit::dpi::PhysicalSize{width, height} = window.inner_size();
+    let aspect_ratio = width as f32 / height as f32;
+    
+    InitOutput {
+        event_loop,
+        window,
+        width,
+        height,
+        aspect_ratio
+    }
+}
+
+pub fn hadnle_windowing(state: &mut State, event: &WindowEvent, control_flow: &mut ControlFlow) {
+    if !state.input(event) {
+        match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => control_flow.set_exit(),
+            WindowEvent::Resized(physical_size) => {
+                state.resize(*physical_size);
+            }
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                // new_inner_size is &mut so w have to dereference it twice
+                state.resize(**new_inner_size);
+            }
+            _ => {}
+        }
+    }
+}
+
+pub fn handle_rendering(state: &mut State, control_flow: &mut ControlFlow) {
+    match state.render() {
+        Ok(_) => {}
+        // Reconfigure the surface if it's lost or outdated
+        Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => state.resize(state.size),
+        // The system is out of memory, we should probably quit
+        Err(wgpu::SurfaceError::OutOfMemory) => control_flow.set_exit(),
+        // We're ignoring timeouts
+        Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+    }
+}
 
 pub async fn run() {
     env_logger::init();
@@ -34,13 +101,13 @@ pub async fn run() {
     };
 
     let mut state = State::new(
-            window, 
-            shader_source, 
-            vertices.as_slice(), 
-            indices, 
-            instances, 
-            camera
-        ).await;
+        window, 
+        shader_source, 
+        vertices.as_slice(), 
+        indices, 
+        instances, 
+        camera
+    ).await;
 
     event_loop.run(move |event, _, control_flow| {
         match event {
@@ -48,40 +115,11 @@ pub async fn run() {
                 ref event,
                 window_id,
             } if window_id == state.window().id() => {
-                if !state.input(event) {
-                    match event {
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            input:
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                                    ..
-                                },
-                            ..
-                        } => control_flow.set_exit(),
-                        WindowEvent::Resized(physical_size) => {
-                            state.resize(*physical_size);
-                        }
-                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                            // new_inner_size is &mut so w have to dereference it twice
-                            state.resize(**new_inner_size);
-                        }
-                        _ => {}
-                    }
-                }
+                crate::hadnle_windowing(&mut state, event, control_flow)
             }
             Event::RedrawRequested(window_id) if window_id == state.window().id() => {
                 state.update();
-                match state.render() {
-                    Ok(_) => {}
-                    // Reconfigure the surface if it's lost or outdated
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => state.resize(state.size),
-                    // The system is out of memory, we should probably quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => control_flow.set_exit(),
-                    // We're ignoring timeouts
-                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
-                }
+                crate::handle_rendering(&mut state, control_flow)
             }
             Event::MainEventsCleared => {
                 // RedrawRequested will only trigger once, unless we manually
